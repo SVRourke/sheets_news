80815db61f452a245ac7b5b7229c041c0b76ec2c
SVRourke <samuelvanrourke@gmail.com>
2021-09-22 17:29:31 -0400

    now parsing rows into actual data

--- script.js
+++ script.js
@@ -29,18 +29,10 @@ const rowsSlector = () => {
 
 // chunk rows takes the list trs created by rowSelector
 // and returns a new list with each items being a list in it's self
-// with the first item being a tr containing the
-// - order number
-// - upvote link?
-// - story link (contains title)
-// - title
-// - sitebit (source site)
-//
-// The second contains
-// - upvote count
-// - user profile link (user name inside)
-// - hide link
-// - comment count
+// with the first item being a tr containing: order number,
+// upvote link, story link (contains title), title, sitebit (source site).
+// The second contains: upvote count, user profile link (user name inside),
+// hide link, comment count
 
 const chunkRows = (rows) => {
   const newRows = [];
@@ -49,7 +41,6 @@ const chunkRows = (rows) => {
   while (rows.length > 0) {
     const currentRow = rows.shift();
     if (currentRow.textContent == "") {
-      console.log("SPACER", tempStorage);
       if (tempStorage.length == 2) newRows.push(tempStorage);
       tempStorage = [];
     } else {
@@ -59,27 +50,44 @@ const chunkRows = (rows) => {
   return newRows;
 };
 
-// with the first item being a tr containing the
-// The second contains
-// - comment count
+const applySelector = (element, selector, url = false) => {
+  try {
+    if (!!url) {
+      return element.querySelector(selector).href;
+    } else {
+      return element.querySelector(selector).textContent;
+    }
+  } catch (TypeError) {
+    return false;
+  }
+};
 
 const parseRow = (row) => {
   return {
-    number: row[0].querySelector(".rank").textContent,
-    upvoteUrl: row[0].querySelector("center > a").href,
-    storyUrl: row[0].querySelector("a.storylink").href,
-    title: row[0].querySelector("a.storylink").textContent,
-    sourceSite: row[0].querySelector("span.sitebit > a").href,
-    sourceName: row[0].querySelector("span.sitebit > a").textContent,
-    upvoteCount: row[1].querySelector(".subtext > span.score"),
-    posterLink: row[1].querySelector("span.subtext > .hnuser").href,
-    pasterName: row[1].querySelector(".subtext > .hnuser").textContent,
-    postAge: row[1].querySelector(".subtext > .age").textContent,
-    hideUrl: row[1].querySelector(".subtext > a:nth-child(5)").href,
-    commentCount: row[1].querySelector(".subtext > a:nth-child(6)")
-      .textContent,
-    commentsUrl: row[1].querySelector(".subtext > a:nth-child(6)").href,
+    number: applySelector(row[0], ".rank"),
+    upvoteUrl: applySelector(row[0], "center > a", true),
+    storyUrl: applySelector(row[0], "a.storylink", true),
+    title: applySelector(row[0], "a.storylink"),
+    sourceSite: applySelector(row[0], "span.sitebit > a", true),
+    sourceName: applySelector(row[0], "span.sitebit > a"),
+    upvoteCount: applySelector(row[1], ".subtext > span.score"),
+    posterLink: applySelector(row[1], ".subtext > .hnuser", true),
+    pasterName: applySelector(row[1], ".subtext > .hnuser"),
+    postAge: applySelector(row[1], ".subtext > .age"),
+    hideUrl: applySelector(row[1], ".subtext > a:nth-child(5)", true),
+    commentCount: applySelector(row[1], ".subtext > a:nth-child(6)"),
+    commentsUrl: applySelector(row[1], ".subtext > a:nth-child(6)", true),
   };
+};
+
+const parsePosts = () => {
+  const rows = nodelistToArray(rowsSlector());
+  const halfParsed = chunkRows(rows);
+  const newRows = [];
+  halfParsed.forEach((e) => {
+    console.log(e);
+    newRows.push(parseRow(e));
+  });
 };
 
 // PAGINATION UTILITIES
@@ -88,5 +96,6 @@ const table = document.querySelector("table#hnmain");
 
 const rows = nodelistToArray(rowsSlector());
 const chunked = chunkRows(rows);
-parseRow(chunked[0]);
 debugger;
+
+parsePosts();
